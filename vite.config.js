c60211dfb52e3f2b4ee4b19ad7ee1ca988999430
KIY7086 <wenyanqi4@gmail.com>
2024-09-17 03:57:14 +0800

    add lockfile

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
  },
- // 如果你需要指定构建输出的基础路径，可以使用 base 选项
  base: '/',
- // 如果你希望服务静态文件，可以使用 publicDir 选项
  publicDir: './public',
 })
