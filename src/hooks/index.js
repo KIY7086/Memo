455864b10e3c2e9db33d16bbfd4de95629e9c2f4
KIY7086 <wenyanqi4@gmail.com>
2024-09-17 04:03:55 +0800

    add handleOutside

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -84,6 +84,24 @@ export const useMemoStorage = () => {
   return { memos, setMemos, currentMemo, setCurrentMemo, saveMemo, createNewMemo };
 };
 
+const handleClickOutside = (event) => {
+  if (
+    sidebarRef.current &&
+    !sidebarRef.current.contains(event.target) &&
+    !event.target.closest(".menu-icon")
+  ) {
+    setIsMenuOpen(false);
+  }
+  if (!event.target.closest(".app-title") && isEditingTitle) {
+    setIsEditingTitle(false);
+    handleSave();
+  }
+};
+
+document.addEventListener("click", handleClickOutside);
+return () => document.removeEventListener("click", handleClickOutside);
+}, [isEditingTitle]);
+
 export const showToast = (message, type = "info") => {
   toast.dismiss();
 
